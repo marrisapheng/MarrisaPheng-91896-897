7500da601e2c9a403988646e8b96e052e8d3f2f0
marrisapheng@gmail.com <marrisa.pheng@my.bdsc.school.nz>
2025-05-22 09:18:35 +1200

    final version1

--- version1.py
+++ version1.py
@@ -21,30 +21,30 @@ def show_menu():
 
     #Menu buttons
     #View summary report button
-    view_summary_report_button = tk.Button(menu_window, text="View Summary Report", command=lambda: [menu_window.destroy(), view_summary_report()])
+    view_summary_report_button= tk.Button(menu_window, text= "View Summary Report", command=lambda: [menu_window.destroy(), view_summary_report()])
     view_summary_report_button.pack(pady=20)
     #Submit summary report button
-    submit_summary_report_button = tk.Button(menu_window, text="Submit Summary Report", command=lambda: [menu_window.destroy(), submit_summary_report()])
+    submit_summary_report_button= tk.Button(menu_window, text= "Submit Summary Report", command=lambda: [menu_window.destroy(), submit_summary_report()])
     submit_summary_report_button.pack(pady=20)
     #Close button
-    close_button = tk.Button(menu_window, text="Close", command=lambda: [menu_window.destroy(), root.destroy()])
+    close_button= tk.Button(menu_window, text= "Close", command=lambda: [menu_window.destroy(), root.destroy()])
     close_button.pack(pady=20)
 
 #Submit summary report
 def submit_summary_report():
-    submit_report = tk.Toplevel(root)
+    submit_report= tk.Toplevel(root)
     submit_report.title("Submit Summary Report")
     submit_report.geometry("1080x1080")
     submit_report.configure(bg="black")
     submit_report.grab_set()
 
     #List for grades
-    grades = []
-    grade_widgets = []
-    result_labels = []
+    grades= []
+    grade_widgets= []
+    result_labels= []
 
     #Name and subjects question
-    tk.Label(submit_report, text="Enter Your Name:", bg="black").pack(pady=10)
+    tk.Label(submit_report, text= "Enter Your Name:", bg="black").pack(pady=10)
     name_entry = tk.Entry(submit_report)
     name_entry.pack(pady=10)
 
@@ -62,7 +62,7 @@ def submit_summary_report():
 
         #Get number of subjects
         try:
-            num_subjects = int(subjects_entry.get())
+            num_subjects= int(subjects_entry.get())
             if num_subjects <= 0 or num_subjects >=6:
                 messagebox.showerror("Invalid Input", "Number of subjects is invalid, please try again.")
                 subjects_entry.delete(0, tk.END)
@@ -74,9 +74,9 @@ def submit_summary_report():
         
         #Enter grade prompt
         for i in range(num_subjects):
-            grade_label = tk.Label(submit_report, text=f"Enter Grade for Subject {i+1}:", bg="black")
+            grade_label= tk.Label(submit_report, text= f"Enter Grade for Subject {i+1}:", bg="black")
             grade_label.pack(pady=10)
-            grade_entry = tk.Entry(submit_report)
+            grade_entry= tk.Entry(submit_report)
             grade_entry.pack(pady=10)
             grades.append(grade_entry)
             grade_widgets.append(grade_label)
@@ -88,7 +88,7 @@ def submit_summary_report():
                 label.destroy()
             result_labels.clear()
             try:
-                grades_list = [int(grade.get()) for grade in grades]
+                grades_list= [int(grade.get()) for grade in grades]
                 if any(grade < 0 or grade > 100 for grade in grades_list):
                     raise ValueError
             except ValueError:
@@ -96,81 +96,81 @@ def submit_summary_report():
                 return
             
             #Calculate average grade
-            average = sum(grades_list) / len(grades_list)
-            average_label = tk.Label(submit_report,text= f"Average Grade: {average:.2f}")
+            average= sum(grades_list) / len(grades_list)
+            average_label= tk.Label(submit_report,text= f"Average Grade: {average:.2f}")
             average_label.pack(pady=10)
 
-            student_name = name_entry.get()
-            summary_text = f"Student Name: {student_name}, Average Grade: {average}\n"
-            summary_label = tk.Label(submit_report, text=summary_text, bg="black")
+            student_name= name_entry.get()
+            summary_text= f"Student Name: {student_name}, Average Grade: {average}\n"
+            summary_label= tk.Label(submit_report, text= summary_text, bg="black")
             summary_label.pack(pady=10)
 
             #Save summary report to txt file
             def save_student_summary():
-                student_name = name_entry.get()
-                if student_name == "":
+                student_name= name_entry.get()
+                if student_name== "":
                     messagebox.showerror("Invalid, Please enter a valid name.")
                     return
                 with open("student_records.txt", "a") as file:
                     file.write(f"Student Name: {student_name}, Average Grade: {average:.2f}\n")
-                save_summary = tk.Label(submit_report, text="Student summary report saved successfully!", bg="black")
+                save_summary= tk.Label(submit_report, text= "Student summary report saved successfully!", bg="black")
                 save_summary.pack(pady=10)
             #Save summary button
-            tk.Button(submit_report, text="Save Summary Report", command=save_student_summary).pack(pady=10)
+            tk.Button(submit_report, text= "Save Summary Report", command=save_student_summary).pack(pady=10)
             #Return to menu
-            tk.Button(submit_report, text="Return to Menu", command=lambda: [submit_report.destroy(), show_menu()]).pack(pady=10)
+            tk.Button(submit_report, text= "Return to Menu", command=lambda: [submit_report.destroy(), show_menu()]).pack(pady=10)
         #Submit button
-        submit_button = tk.Button(submit_report, text="Submit Grades", command=submit_grades)
+        submit_button= tk.Button(submit_report, text= "Submit Grades", command=submit_grades)
         submit_button.pack(pady=10)
         grade_widgets.append(submit_button)
 
     #Button from entering subjects to entering grades
-    next_button = tk.Button(submit_report, text="Next", command=grade_entries)
+    next_button= tk.Button(submit_report, text= "Next", command=grade_entries)
     next_button.pack(pady=10)
 
     #Return to menu button
-    return_button = tk.Button(submit_report, text="Return to Menu", command=lambda: [submit_report.destroy(),show_menu()])
+    return_button= tk.Button(submit_report, text= "Return to Menu", command=lambda: [submit_report.destroy(),show_menu()])
     return_button.pack(pady=10)
 
 #View summary report
 def view_summary_report():
-    view_report = tk.Toplevel(root)
+    view_report= tk.Toplevel(root)
     view_report.title("View Summary Report")
     view_report.geometry("1080x1080")
     view_report.configure(bg="black")
     view_report.grab_set()
 
     #Enter student name prompt
-    name_prompt_label = tk.Label(view_report, text="Enter Student Name:", bg="black")
+    name_prompt_label= tk.Label(view_report, text="Enter Student Name:", bg="black")
     name_prompt_label.pack(pady=10)
 
     #Search entry
-    search_name_entry = tk.Entry(view_report)
+    search_name_entry= tk.Entry(view_report)
     search_name_entry.pack(pady=10)
 
-    result_label = tk.Label(view_report, text="", bg="black")
+    result_label= tk.Label(view_report, text= "", bg="black")
     result_label.pack(pady=10)
-
+# HERE - WHEN SEARCHING FOR STUDENT NAME, IT DOES NOT SHOW THE STUDENT NAME, INSTEAD "ALERT" IS SHOWN
     def search_student():
-        student_name = search_name_entry.get().strip()
-        if student_name == "":
+        student_name= search_name_entry.get().strip()
+        if student_name== "":
             messagebox.showerror("Invalid Input", "Please enter a valid name.")
             return
-        found = False
+
         try:
             with open("student_records.txt", "r") as file:
-                for line in file:
-                    if student_name in line:
-                        result_label.config(text=line)
-                        found = True
-                        break
+                student_found = [line for line in file if student_name.lower() in line.lower()]
+                if student_found:
+                    result_text = "\n".join(student_found)
+                    result_label.config(text=result_text)
+                    return
         except FileNotFoundError:
             result_label.config(text = "Student not found")
             return
-        if not found:
-            messagebox.showerror("Student not found, please try again.")
-            search_name_entry.delete(0, tk.END)
-            result_label.config(text="")
+        #If student was not found
+        messagebox.showerror("Student not found, please try again.")
+        search_name_entry.delete(0, tk.END)
+        result_label.config(text="")
                 
     #Search button
     search_button = tk.Button(view_report, text="Search", command=search_student)
@@ -179,8 +179,6 @@ def view_summary_report():
     #Return to menu button
     return_button = tk.Button(view_report, text="Return to Menu", command=lambda: [view_report.destroy(), show_menu()])
     return_button.pack(pady=10)
-
-
 
 
 show_menu()
